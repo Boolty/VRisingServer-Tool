65ae59be37579d3d85ca9978c87e5f2492500628
Alala <raffaello@hotmail.de>
2022-06-01 08:54:04 +0200

    Update ServerTool.py
    Server Performance increase

--- ServerTool.py
+++ ServerTool.py
@@ -6,6 +6,8 @@ import threading
 from datetime import datetime
 
 
+PROCNAME = 'VRisingServer.exe'
+
 def checkIfProcessRunning(processName):
     '''
     Check if there is any running process that contains the given name processName.
@@ -22,15 +24,28 @@ def checkIfProcessRunning(processName):
 
 def launch():
     print('Starting VRisingServer...')
-    print('Version: 1.3')
-    os.system('VRisingServer.exe')
+    print('Version: 1.4')
+    global PROCNAME
+    os.system(PROCNAME)
     time.sleep(20)
+    for proc in psutil.process_iter():
+        if proc.name() == PROCNAME:
+            proc.nice(psutil.REALTIME_PRIORITY_CLASS)
+
+def up():
+    global PROCNAME
+    while True:
+        time.sleep(60)
+        for proc in psutil.process_iter():
+            if proc.name() == PROCNAME:
+                proc.nice(psutil.REALTIME_PRIORITY_CLASS)
 
 def main():
+    global PROCNAME
     print('Stop the Server tool with STRG+C')
     ctypes.windll.kernel32.SetConsoleTitleW("VRisingServerTool by Boolty")
+    off = True
     time.sleep(3)
-    PROCNAME = 'VRisingServer.exe'
     restart = '03:00'
     while True:
         if checkIfProcessRunning(PROCNAME):
@@ -42,11 +57,14 @@ def main():
                 for proc in psutil.process_iter():
                     if proc.name() == PROCNAME:
                         proc.kill()
-            time.sleep(20)
+            time.sleep(40)
         else:
             print('VRisingServer will not run...')
             print('Starting VRisingServer...')      
             threading.Thread(target=launch).start()  
+            if off == True:
+                threading.Thread(target=up).start()  
+                off = False 
             time.sleep(10)
 
 
